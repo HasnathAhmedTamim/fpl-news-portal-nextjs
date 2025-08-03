7772122ebbe932d1eecf835450c3651f1dfb37cc
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2025-08-03 17:49:09 +0600

    Mobile menu add

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,9 +1,18 @@
+'use client'
 import Link from "next/link"
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "../ui/navigation-menu"
 import { Switch } from "../ui/switch"
 import { Button } from "../ui/button"
-import { AiOutlineMenu } from "react-icons/ai";
+import { usePathname } from "next/navigation"
+import MobileMenu from "./MobileMenu"
+
+
+
+
+
 const Navbar = () => {
+    let pathname = usePathname();
+
     return (
         <header className="py-4  shadow-md">
             <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
@@ -16,26 +25,26 @@ const Navbar = () => {
                     <NavigationMenuList >
                         <NavigationMenuItem className="flex items-center space-x-8">
 
-                            <NavigationMenuLink href="/news" className="hover:text-red-700">FPL News</NavigationMenuLink>
+                            <Link href="/news" className={` ${pathname === "/news" ? "text-red-700 font-semibold" : "text-gray-800 "} hover:text-red-700`}>FPL News</Link>
 
                         </NavigationMenuItem>
                         <NavigationMenuItem>
-                            <NavigationMenuTrigger className=" hover:text-red-700 text-base">FPL Service</NavigationMenuTrigger>
+                            <NavigationMenuTrigger className={` ${pathname === "/fplservices" ? "text-red-700 font-semibold" : "text-gray-800 "} hover:text-red-700`}>FPL Service</NavigationMenuTrigger>
                             <NavigationMenuContent >
                                 <div >
                                     <ul className="text-gray-800 rounded-md shadow-md px-4  space-y-2">
-                                        <li><NavigationMenuLink href="/fpl-services/link-1" >Link1</NavigationMenuLink></li>
-                                        <li><NavigationMenuLink href="/fpl-services/link-2">Link2</NavigationMenuLink></li>
-                                        <li><NavigationMenuLink href="/fpl-services/link-3" >Link3</NavigationMenuLink></li>
+                                        <li><Link href="/fplservices/link1" >Link1</Link></li>
+                                        <li><Link href="/fplservices/link2">Link2</Link></li>
+                                        <li><Link href="/fplservices/link3" >Link3</Link></li>
                                     </ul>
                                 </div>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
                         <NavigationMenuItem className="flex items-center space-x-8">
 
-                            <NavigationMenuLink href="/aboutfpl" className="hover:text-red-700">About FPL</NavigationMenuLink>
+                            <Link href="/aboutfpl" className={` ${pathname === "/aboutfpl" ? "text-red-700 font-semibold" : "text-gray-800 "} hover:text-red-700`}>About FPL</Link>
 
-                            <NavigationMenuLink href="/contactfpl" className="hover:text-red-700">Contact FPL</NavigationMenuLink>
+                            <Link href="/contactfpl" className={` ${pathname === "/contactfpl" ? "text-red-700 font-semibold" : "text-gray-800 "} hover:text-red-700`}>Contact FPL</Link>
 
                         </NavigationMenuItem>
 
@@ -57,9 +66,8 @@ const Navbar = () => {
                 </div>
 
                 {/* mobile menu */}
-                <div className="lg:hidden">
-                    <Button variant="outline"><AiOutlineMenu size={24} /></Button>
-                </div>
+
+                <MobileMenu></MobileMenu>
 
             </nav>
         </header>
